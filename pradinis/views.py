3ba54d12efd4ddb0fbfc8ff87263aefe52e48cb9
Audrius <adrdei@inbox.lt>
2022-03-26 18:02:32 +0200

    pradinio iskelimas

--- pradinis/views.py
+++ pradinis/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-def registracija(request):
+# Create your views here.
+def pradinis(request):
 #    return HttpResponse("Hello, world. You're at the registracija page.")
-    return render(request, 'registracija.html')
-
-
+    return render(request, 'pradinis.html')
